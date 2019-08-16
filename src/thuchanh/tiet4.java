0d0eeadfa7b8a58d6b0c67d18b04f485aeee9134
admin <admin@TINHPH.net.fpt>
2019-08-16 09:13:17 +0700

    initil

---
+++ src/thuchanh/tiet4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thuchanh;
+
+/**
+ *
+ * @author admin
+ */
+public class tiet4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Họ và tên: Nguyễn Văn Nam");
+        System.out.println("Sinh ngày 12/12/2004");
+        System.out.println("Sở thích: Uống trà sữa Gong Cha free");
+    }
+    
+}
